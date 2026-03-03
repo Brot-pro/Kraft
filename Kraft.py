507626a5bf2eb3f36d8ea75ddd2f254f1ea4e531
Brot-pro <Brot_pro@outlook.com>
2026-03-03 21:00:54 +0100

    Kraftaddieren + kräfteparalellogramm überarbeitet / fertig

--- Kraft.py
+++ Kraft.py
@@ -13,6 +13,14 @@ class Test(Scene):
 
 class Kräftaddieren(Scene):
     def construct(self):
+        kräfteaddieren = Tex("Kräfte addieren").scale(1.2)
+        ul1 = Underline(kräfteaddieren)
+        blackbox1 = SurroundingRectangle(kräfteaddieren, buff=0, color=BLACK,fill_opacity=1)
+        kräftaddierengruppe = VGroup(blackbox1,kräfteaddieren,ul1)
+        self.add(kräftaddierengruppe)
+        self.wait()
+        self.play(kräftaddierengruppe.animate.to_corner(UL))
+
         nupl = NumberPlane(x_range=(-10,10,1.5),y_range=(-6,6,1.5)).set_opacity(0.5)
         nupl.shift(DL)
         vector1 = nupl.get_vector([0,0,0]).set_color(ORANGE)
@@ -26,8 +34,18 @@ class Kräftaddieren(Scene):
         fragezeichen.add_updater(lambda mob : mob.next_to(vector3, UR).shift(0.6 *DOWN))
         self.wait(0.5)
         self.play(FadeIn(nupl))
+        self.add(kräftaddierengruppe)
         self.play(vector1.animate.become(nupl.get_vector([vector1x,vector1y,0]).set_color(ORANGE)),vector2.animate.become(nupl.get_vector([vector2x,vector2y]).set_color(BLUE)),run_time=1)
         self.wait(2)
+
+        pfeil = Tex("Pfeil = Vektor")
+        blackbox2 = SurroundingRectangle(pfeil, color=BLACK,fill_opacity=1,buff=0)
+        pfeilgruppe = VGroup(blackbox2,pfeil)
+        pfeilgruppe.shift(1.5 * DR)
+        self.play(Write(pfeilgruppe))
+        self.wait()
+        self.play(pfeilgruppe.animate.next_to(kräftaddierengruppe, DOWN).to_edge(LEFT))
+
         self.add(fragezeichen)
         self.play(vector3.animate.become(nupl.get_vector([2.5,3.2,0]).set_color(WHITE)),run_time=2)
         self.wait(0.5)
@@ -38,55 +56,120 @@ class Kräftaddieren(Scene):
         self.play(vector3.animate.become(nupl.get_vector([0,0,0]).set_color(WHITE)),FadeOut(fragezeichen),run_time=2)
         self.wait(2)
         self.play(vector2.animate.shift(vector1y*UP).shift(vector1x*RIGHT))
-        self.wait()
+
+        vektorverbindung = Tex("Verbindung")
+        vektorverbindungfull = Tex("Vektoren werden Verbunden")
+        blackbox4 = SurroundingRectangle(vektorverbindungfull, color=BLACK,fill_opacity=1,buff=0)
+        vektorverbindungfullgruppe = VGroup(blackbox4,vektorverbindungfull)
+        vektorverbindungfullgruppe.move_to([-4.8,1.8,1])
+        blackbox3 = SurroundingRectangle(vektorverbindung, color=BLACK,fill_opacity=1,buff=0)
+        vektorverbindunggruppe = VGroup(blackbox3,vektorverbindung)
+        punkt = Dot()
+        vektorverbindunggruppe.shift(1.2 * RIGHT)
+        self.play(Write(vektorverbindunggruppe))
+        punkt = Dot()
+        punkt.move_to(vector2.get_start())
+        self.play(Circumscribe(punkt,Circle),run_time=1.4)
+        self.play(vektorverbindunggruppe.animate.move_to([-5,1.8,1]).set_opacity(0))
+        vektorverbindungfullgruppe.next_to(pfeilgruppe, DOWN).to_edge(LEFT)
+        self.play(FadeIn(vektorverbindungfullgruppe))
+
         self.play(vector3.animate.become(nupl.get_vector([vector1x+vector2x,vector1y+vector2y,0]).set_color(WHITE)),run_time=2)
+
+        resultierend = Tex("Resultierende Kraft")
+        blackbox6 = SurroundingRectangle(resultierend, color=BLACK,fill_opacity=1,buff=0)
+        resultierendgruppe = VGroup(blackbox6,resultierend)
+        resultierendgruppe.move_to([-2.3,0,0])
+        self.play(Write(resultierendgruppe))
+        self.play(vector3.animate.set_stroke(width=8,color=YELLOW),rate_func=there_and_back)
+        self.play(resultierendgruppe.animate.next_to(vektorverbindungfullgruppe, DOWN).to_edge(LEFT))
+
         self.play(vector1.animate.become(nupl.get_vector([vector1x,vector1y,0]).set_color(ORANGE)),vector2.animate.become(nupl.get_vector([vector2x,vector2y]).set_color(BLUE)),run_time=2)
         self.wait(2)
-        vector1x = -4.5
+        vector1x = 3
         vector1y = -1.5
-        vector2x = 2.75
-        vector2y = 4.5
-        self.play(vector1.animate.become(nupl.get_vector([vector1x,vector1y,0]).set_color(ORANGE)),vector2.animate.become(nupl.get_vector([vector2x,vector2y]).set_color(BLUE)),vector3.animate.become(nupl.get_vector([0,0,0]).set_color(WHITE)),run_time=2)
+        vector2x = 4.5
+        vector2y = 1.5
+        basevector1 = nupl.get_vector([vector1x,vector1y,0]).set_color(ORANGE)
+        basevector2 = nupl.get_vector([vector2x,vector2y,0]).set_color(BLUE)
+        self.play(vector1.animate.become(basevector1),vector2.animate.become(basevector2),vector3.animate.become(nupl.get_vector([0,0,0]).set_color(WHITE)),run_time=2)
         self.wait()
         self.play(vector2.animate.shift(vector1y*UP).shift(vector1x*RIGHT))
         self.play(vector3.animate.become(nupl.get_vector([vector1x+vector2x,vector1y+vector2y,0]).set_color(WHITE)),run_time=2)
-        self.play(vector1.animate.become(nupl.get_vector([vector1x,vector1y,0]).set_color(ORANGE)),vector2.animate.become(nupl.get_vector([vector2x,vector2y]).set_color(BLUE)),run_time=2)
-        vector1x = -3
+        self.play(vector1.animate.become(basevector1),vector2.animate.become(basevector2),run_time=2)
+        vector1x = -1.5
         vector1y = 0.75
         vector2x = 4.5
         vector2y = 3
+        basevector1 = nupl.get_vector([vector1x,vector1y,0]).set_color(ORANGE)
+        basevector2 = nupl.get_vector([vector2x,vector2y,0]).set_color(BLUE)
         self.wait()
-        self.play(vector1.animate.become(nupl.get_vector([vector1x,vector1y,0]).set_color(ORANGE)),vector2.animate.become(nupl.get_vector([vector2x,vector2y]).set_color(BLUE)),vector3.animate.become(nupl.get_vector([vector1x+vector2x,vector1y+vector2y,0]).set_color(WHITE)),run_time=2)
+
+        füralle = Tex("Vektoren: Selber Angriffpunkt")
+        blackbox5 = SurroundingRectangle(füralle, color=BLACK,fill_opacity=1,buff=0)
+        fürallegruppe = VGroup(blackbox5,füralle)
+        fürallegruppe.next_to(resultierendgruppe, DOWN).to_edge(LEFT)
+        self.play(FadeIn(fürallegruppe))
+
+        self.play(vector1.animate.become(basevector1),vector2.animate.become(basevector2),vector3.animate.become(nupl.get_vector([vector1x+vector2x,vector1y+vector2y,0]).set_color(WHITE)),run_time=2)
         vector1x = -4.5
-        vector1y = 1.5
+        vector1y = 0.75
         vector2x = 1.5
         vector2y = -2.75
+        basevector1 = nupl.get_vector([vector1x,vector1y,0]).set_color(ORANGE)
+        basevector2 = nupl.get_vector([vector2x,vector2y,0]).set_color(BLUE)
         self.wait()
-        self.play(vector1.animate.become(nupl.get_vector([vector1x,vector1y,0]).set_color(ORANGE)),vector2.animate.become(nupl.get_vector([vector2x,vector2y]).set_color(BLUE)),vector3.animate.become(nupl.get_vector([vector1x+vector2x,vector1y+vector2y,0]).set_color(WHITE)),run_time=2)
+        self.play(vector1.animate.become(basevector1),vector2.animate.become(basevector2),vector3.animate.become(nupl.get_vector([vector1x+vector2x,vector1y+vector2y,0]).set_color(WHITE)),run_time=2)
         self.wait()
 
 class Kräfteparalellogramm(Scene):
     def construct(self):
+
+        kräfteparalellogramm = Tex("Kräfteparalellogramm").scale(1.2)
+        ul1 = Underline(kräfteparalellogramm)
+        blackbox1 = SurroundingRectangle(kräfteparalellogramm, buff=0, color=BLACK,fill_opacity=1)
+        kräfteparalellogrammgruppe = VGroup(blackbox1,kräfteparalellogramm,ul1)
+        self.add(kräfteparalellogrammgruppe)
+        self.wait()
+        self.play(kräfteparalellogrammgruppe.animate.to_corner(UL))
+
+
         nupl = NumberPlane(x_range=(-10,10,1.5),y_range=(-6,6,1.5)).set_opacity(0.5)
         nupl.shift(2*DL)
+        nupl.shift(LEFT)
         vector1 = nupl.get_vector([0,0,0]).set_color(ORANGE)
         vector2 = nupl.get_vector([0,0,0]).set_color(BLUE)
         vector3 = nupl.get_vector([0,0,0]).set_color(WHITE)
-        text = Tex("Kräfteparalellogramm")
         vector1x = 4.5
         vector1y = 1.5
         vector2x = 1.5
         vector2y = 3
         vector4 = nupl.get_vector([vector2x,vector2y,0]).set_color(BLUE)
         vector5 = nupl.get_vector([vector1x,vector1y,0]).set_color(ORANGE)
-        self.wait(0.5)
+
         self.play(FadeIn(nupl))
+        self.add(kräfteparalellogrammgruppe)
         self.play(vector1.animate.become(nupl.get_vector([vector1x,vector1y,0]).set_color(ORANGE)),vector2.animate.become(nupl.get_vector([vector2x,vector2y]).set_color(BLUE)),run_time=1)
         self.wait()
         self.play(vector2.animate.shift(vector1y*UP).shift(vector1x*RIGHT))
         self.play(vector3.animate.become(nupl.get_vector([vector1x+vector2x,vector1y+vector2y,0]).set_color(WHITE)),run_time=2)
+
+        additionbasisfull = Tex("Basis auf Kraftaddition")
+        blackbox2 = SurroundingRectangle(additionbasisfull, color=BLACK,fill_opacity=1,buff=0)
+        additionbasisfullgruppe = VGroup(blackbox2,additionbasisfull)
+        additionbasis = Tex("Kraftaddition")
+        blackbox3 = SurroundingRectangle(additionbasis, color=BLACK,fill_opacity=1,buff=0)
+        additionbasisgruppe = VGroup(blackbox3,additionbasis)
+        additionbasisgruppe.shift(2.5 * LEFT)
+        self.play(Write(additionbasisgruppe))
+        self.play(vector3.animate.set_stroke(width=8,color=YELLOW),rate_func=there_and_back)
+        self.play(additionbasisgruppe.animate.move_to([-4.5,2,1]).set_opacity(0))
+        additionbasisfullgruppe.next_to(kräfteparalellogrammgruppe, DOWN).to_edge(LEFT)
+        self.play(FadeIn(additionbasisfullgruppe))
+        self.wait(0.6)
+
         #ist gehardcoded ich weiß....
-        punkt = Dot(point=[4,2.5,0],color=RED)
+        punkt = Dot(point=[3,2.5,0],color=RED)
         self.play(FadeIn(punkt))
         self.wait(2)
         self.play(vector3.animate.become(nupl.get_vector([0,0,0]).set_color(WHITE)))
@@ -97,21 +180,101 @@ class Kräfteparalellogramm(Scene):
         self.play(vector1.animate.become(nupl.get_vector([vector1x,vector1y,0]).set_color(ORANGE)),vector2.animate.become(nupl.get_vector([vector2x,vector2y]).set_color(BLUE)),run_time=1)
         self.wait()
         self.play(vector1.animate.shift(vector2y*UP).shift(vector2x*RIGHT))
-        self.play(vector3.animate.become(nupl.get_vector([vector1x+vector2x,vector1y+vector2y,0]).set_color(WHITE)),run_time=2)
-        self.wait(2)
-        text.shift(2.8*UP)
-        text.shift(0.7*RIGHT)
-        self.play(Write(text),run_time=0.8)
-        self.wait(0.2)
-        self.play(Circumscribe(text))
-        self.wait(2)
 
+        spieglung = Tex("Spieglung")
+        blackbox2 = SurroundingRectangle(spieglung, color=BLACK,fill_opacity=1,buff=0)
+        spieglunggruppe = VGroup(blackbox2,spieglung)
+        spieglungfull = Tex("Spieglung der Vektoren")
+        blackbox4 = SurroundingRectangle(spieglungfull, color=BLACK,fill_opacity=1,buff=0)
+        spieglungfullgruppe = VGroup(blackbox4,spieglungfull)
+        spieglunggruppe.move_to([-3.7,-0.7,0])
+        self.play(Write(spieglunggruppe))
+        self.play(vector1.animate.set_stroke(width=8,color=YELLOW),vector2.animate.set_stroke(width=8,color=YELLOW),rate_func=there_and_back)
+        self.play(spieglunggruppe.animate.move_to([-4.5,1.6,1]).set_opacity(0))
+        spieglungfullgruppe.next_to(additionbasisfullgruppe, DOWN).to_edge(LEFT)
+        self.play(FadeIn(spieglungfullgruppe))
+        self.play(FadeOut(punkt))
+        
+        vector1.become(nupl.get_vector([vector1x,vector1y]))
+        vector4.become(nupl.get_vector([vector2x,vector2y]).set_color(BLUE))
+        vector4.shift(vector1y*UP).shift(vector1x*RIGHT)
+        vector5.become(nupl.get_vector([vector1x,vector1y]).set_color(ORANGE))
+        vector1.shift(vector2y*UP).shift(vector2x*RIGHT).set_color(ORANGE)
+        vector2.become(nupl.get_vector([vector2x,vector2y]).set_color(BLUE))
+        self.play(vector3.animate.become(nupl.get_vector([vector1x+vector2x,vector1y+vector2y,0]).set_color(WHITE)))
+
+        self.wait()
+        vector1x = 3
+        vector1y = 0
+        vector2x = 3
+        vector2y = 3
+        self.play(
+        vector1.animate.become(nupl.get_vector([vector1x,vector1y]).shift(vector2y*UP + vector2x*RIGHT).set_color(ORANGE)),
+        vector4.animate.become(nupl.get_vector([vector2x,vector2y]).shift(vector1y*UP + vector1x*RIGHT).set_color(BLUE)),
+        vector5.animate.become(nupl.get_vector([vector1x,vector1y]).set_color(ORANGE)),
+        vector2.animate.become(nupl.get_vector([vector2x,vector2y]).set_color(BLUE)),
+        vector3.animate.become(nupl.get_vector([vector1x+vector2x,vector1y+vector2y,0]).set_color(WHITE)),
+        run_time=1.5)
+
+        füralle = Tex("Vektoren: Selber Angriffpunkt")
+        blackbox6 = SurroundingRectangle(füralle, color=BLACK,fill_opacity=1,buff=0)
+        fürallegruppe = VGroup(blackbox6,füralle)
+        fürallegruppe.next_to(spieglungfullgruppe, DOWN).to_edge(LEFT)
+
+        self.wait()
+        vector1x = -3
+        vector1y = -0.75
+        vector2x = 1.5
+        vector2y = 2.25
+        self.play(
+        vector1.animate.become(nupl.get_vector([vector1x,vector1y]).shift(vector2y*UP + vector2x*RIGHT).set_color(ORANGE)),
+        vector4.animate.become(nupl.get_vector([vector2x,vector2y]).shift(vector1y*UP + vector1x*RIGHT).set_color(BLUE)),
+        vector5.animate.become(nupl.get_vector([vector1x,vector1y]).set_color(ORANGE)),
+        vector2.animate.become(nupl.get_vector([vector2x,vector2y]).set_color(BLUE)),
+        vector3.animate.become(nupl.get_vector([vector1x+vector2x,vector1y+vector2y,0]).set_color(WHITE)),
+        FadeIn(fürallegruppe),
+        run_time=1.5)
+
+        self.wait()
+        vector1x = 6
+        vector1y = -1.5
+        vector2x = 1.5
+        vector2y = 1.5
+        self.play(
+        vector1.animate.become(nupl.get_vector([vector1x,vector1y]).shift(vector2y*UP + vector2x*RIGHT).set_color(ORANGE)),
+        vector4.animate.become(nupl.get_vector([vector2x,vector2y]).shift(vector1y*UP + vector1x*RIGHT).set_color(BLUE)),
+        vector5.animate.become(nupl.get_vector([vector1x,vector1y]).set_color(ORANGE)),
+        vector2.animate.become(nupl.get_vector([vector2x,vector2y]).set_color(BLUE)),
+        vector3.animate.become(nupl.get_vector([vector1x+vector2x,vector1y+vector2y,0]).set_color(WHITE)),
+        run_time=1.7)
+
+
+
+
+
+
+
+        # self.wait()
+        # vector1x = 3
+        # vector1y = 1.5
+        # vector2x = 1.5
+        # vector2y = 3
+
+        # vector1.become(nupl.get_vector([vector1x,vector1y])),
+        # vector4.become(nupl.get_vector([vector2x,vector2y]).set_color(BLUE))
+        # self.play(
+        # vector4.animate.shift(vector1y*UP).shift(vector1x*RIGHT),
+        # vector5.animate.become(nupl.get_vector([vector1x,vector1y]).set_color(ORANGE)),
+        # vector1.animate.shift(vector2y*UP).shift(vector2x*RIGHT).set_color(ORANGE),
+        # vector2.animate.become(nupl.get_vector([vector2x,vector2y]).set_color(BLUE)),
+        # vector3.animate.become(nupl.get_vector([vector1x+vector2x,vector1y+vector2y,0]).set_color(WHITE)))
 
         
+        self.wait()
+
 # s = [2,1]
 # func1 = lambda pos: s[0] * RIGHT + s[1] * UP
 # vecfield = ArrowVectorField(func1,x_range=[-9,8,0.7],y_range=[-5,4,0.7]).set_color(BLUE).set_opacity(0)
 # self.add(vecfield)
 # self.play(vecfield.animate.shift(s[0]/2 * RIGHT + s[1]/2 * UP).set_opacity(1), rate_func=rate_functions.ease_in_quart, run_time=1.5)
 # self.play(vecfield.animate.shift(s[0]/2 * RIGHT + s[1]/2 * UP).set_opacity(0), rate_func=rate_functions.ease_out_quart, run_time=1.5)
-
